e8c89f60db1e24beece7938cc052f2e5cdaa9d5d
OkiahOoo <sky_kiah@163.com>
2023-03-30 17:04:33 +0800

    初始化项目

---
+++ design-pattern/src/main/java/org/kiah/designpattern/singleton/HungrySingleton.java
@@ -0,0 +1,13 @@
+package org.kiah.designpattern.singleton;
+
+// 饿汉式单例
+public class HungrySingleton {
+    private static final HungrySingleton instance = new HungrySingleton();
+
+    private HungrySingleton() {
+    }
+
+    public static HungrySingleton getInstance() {
+        return instance;
+    }
+}
---
+++ design-pattern/src/main/java/org/kiah/designpattern/singleton/LazySingleton.java
@@ -0,0 +1,16 @@
+package org.kiah.designpattern.singleton;
+
+// 懒汉式单例
+public class LazySingleton {
+    private static LazySingleton instance = null;
+
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
